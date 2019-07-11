4983cfae5e77c0fb97fb474561e303b1edecd608
Lanna <Lanna_lmb@hotmail.com>
2019-07-10 22:05:31 -0400

    start of program

---
+++ HatDrawing/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HatDrawing.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "A brief description of how the virtual hat game works: ";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Lanna Brasure";
+
+            return View();
+        }
+    }
+}
