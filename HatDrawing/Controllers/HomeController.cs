e3925aa2f100871f60f2126a229338a7419fcade
Lanna <Lanna_lmb@hotmail.com>
2019-07-14 21:24:24 -0400

    added code for duplicate name, drawing name, adding name

--- HatDrawing/Controllers/HomeController.cs
+++ HatDrawing/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +9,27 @@ namespace HatDrawing.Controllers
 {
     public class HomeController : Controller
     {
+        // Home Page
         public ActionResult Index()
         {
+            // Declare ArrayList and Variables
+            ArrayList Names = new ArrayList();
+            string lc_name = name.ToLower();
+
+            // if the name has already been entered, we will tell the user that.
+            if (Names.Contains(lc_name))
+            {
+                return RedirectToAction("/Home/DuplicateName");
+            }
+
+            // if the name has not been entered, we will add the name to the list.
+            Names.Add(lc_name);
+
             return View();
         }
 
+
+        // Informational Page
         public ActionResult About()
         {
             ViewBag.Message = "A brief description of how the virtual hat game works: ";
@@ -20,6 +37,8 @@ namespace HatDrawing.Controllers
             return View();
         }
 
+
+        // Contact Information
         public ActionResult Contact()
         {
             ViewBag.Message = "Lanna Brasure";
@@ -27,14 +46,45 @@ namespace HatDrawing.Controllers
             return View();
         }
 
-        public ActionResult AddName(string name)
+
+        //  Return View for Name Added
+        public ActionResult AddName(string lc_name)
         {
-            ViewBag.Name = name;
+            ViewBag.Name = lc_name;
 
             return View();
+        }
 
 
+        // Draw a Random Name from the Hat
+        public ActionResult DrawName(ArrayList Names)
+        {
+            Random random = new Random();
+            int rand_element = random.Next(Names.Count);
+            string name_drawn = Names[rand_element].ToString();
+  
+            Names.RemoveAt(rand_element);
 
+            ViewBag.NameDrawn = name_drawn;
+            return RedirectToAction("/Home/NameDrawn");
+        }
+
+
+        // Return View for Duplicate Name
+        public ActionResult DuplicateName(string lc_name)
+        {
+            ViewBag.DuplicateName = lc_name;
+
+            return View();
+        }
+
+
+        // Return View for Name Drawn
+        public ActionResult NameDrawn(string lc_name)
+        {
+            ViewBag.NameDrawn = lc_name;
+
+            return View();
         }
     }
 }
