2ccf2f3c6becfa7e871fcb1ec1b81617178aff11
Lanna <Lanna_lmb@hotmail.com>
2019-07-22 14:54:14 -0400

    updated home page, cleaned up output, added code to redirect name drawn page after 5 seconds

--- HatDrawing/Controllers/HomeController.cs
+++ HatDrawing/Controllers/HomeController.cs
@@ -2,9 +2,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
-using System.Web.Routing;
 using System.Windows.Forms;
 
 namespace HatDrawing.Controllers
@@ -40,25 +40,30 @@ namespace HatDrawing.Controllers
         //  Return View for Name Added
         public ActionResult AddName(string name)
         {
-            // Store the name and arraylist in a session variable
+            // store our ArrayList in a session variable so we can retrieve it later
             var names = HttpContext.Session["Names"] as ArrayList;
 
+            // we want to make sure we create a new ArrayList() if names is null
             if (names == null) names = new ArrayList();
    
-            string lc_name = name.ToLower();
+            // lowercase the name for better comparison and trim whitepspace
+            string lc_name = name.ToLower().Trim();
 
-            // redirect to DuplicateName page if the name exists in the arraylist
+            // redirect to DuplicateName page if the name already exists in the ArrayList
             if (names.Contains(lc_name))
             {
-                // pass the session variable name for when we redirect the user to the duplicate name page
+                // pass the session variable name for when we redirect the user to the DuplicateName page
+                // this is just so that we can display the duplicate name entered
                 return RedirectToAction("DuplicateName", new { name = name });
             }
 
+            // add the name to the ArrayList
             names.Add(lc_name);
 
-            ViewBag.AddNameMessage = "The Name " + name + " was Added to the Hat!";
+            // store a message to be displayed showing the user that the name was added to the hat
+            ViewBag.AddNameMessage = "The Name " + lc_name + " was Added to the Hat!";
 
-
+            // setting the session variable back to the ArrayList
             HttpContext.Session["Names"] = names;
 
             return View();
@@ -69,21 +74,33 @@ namespace HatDrawing.Controllers
         public ActionResult DuplicateName(string name)
         {
 
-            ViewBag.DuplicateNameMessage = "The name " + name + " has already been entered into the hat.  Please try again.";
+            ViewBag.DuplicateNameMessage = "The name " + name + " has already been entered into the hat.  Please enter a different name.";
             return View();
         }
 
 
         // Draw a Random Name from the Hat
-        public ActionResult DrawName(ArrayList names)
+        public ActionResult DrawName()
         {
+            var names = HttpContext.Session["Names"] as ArrayList;
+
+            // if there are no names entered yet - we cannot draw a name
+            if (names == null)
+            {
+                ViewBag.NameDrawnTitle = "No Names Entered";
+                ViewBag.NameDrawnMessage = "You must enter a name before you can draw a name.";
+                return View();
+            }
+
+            // get a random name from the ArrayList
             Random random = new Random();
             int rand_element = random.Next(names.Count);
             string name_drawn = names[rand_element].ToString();
   
             names.RemoveAt(rand_element);
 
-            ViewBag.NameDrawnMessage = "The Name Drawn was: " + name_drawn + "!";
+            ViewBag.NameDrawnTitle = "We Have a Winner!";
+            ViewBag.NameDrawnMessage = "The Name Drawn was: " + name_drawn;
 
             return View();
         }
